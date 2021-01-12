aa7192677e86575f483f82cefa210bffd5cc8ceb
prem <premplayz@gmail.com>
2021-01-12 14:58:31 +0700

    Add project files.

---
+++ Assignment 2 Bank Account/Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2_Bank_Account
+{
+    class Account
+    {
+        private string username;
+        public double balance; 
+
+        public Account(string username, double balance)
+        {
+            this.username = username;
+            this.balance = balance;
+        }
+        public double CheckBalance()
+        {
+            return balance;
+        }
+        public double WithdrawMoney(double MoneyOut)
+        {
+            if (MoneyOut > balance)
+            {
+                Console.WriteLine("Poor lol");
+            }
+            else if (MoneyOut > 0)
+            {
+                Console.WriteLine("Withdraw Successful");
+                balance = balance - MoneyOut;
+            }
+            else
+            {
+                Console.WriteLine("Negative Numbers!");
+            }
+            return balance;
+        }
+        public double DepositMoney(double MoneyIn)
+        {
+            if (MoneyIn > 0)
+            {
+                Console.WriteLine("Deposit Successful, Money added");
+                balance = balance + MoneyIn;
+            }
+            else
+            {
+                Console.WriteLine("Deposit Error, Scam attempt");
+            }
+            return balance;
+        }
+        // Interest in format 1.05
+        public double AddInterest(double InterestRate) 
+        {
+            string date = DateTime.Now.ToShortDateString();
+            Console.WriteLine(date);
+            if (date == "12/01/2021")
+            {
+                balance = balance * InterestRate;
+            }
+            return balance;
+        }
+    }
+}
---
+++ Assignment 2 Bank Account/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assignment_2_Bank_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account InTouch = new Account("Inn", 25);
+            Console.WriteLine(InTouch.CheckBalance());
+            Console.WriteLine(InTouch.DepositMoney(10));
+        }
+    }
+}
